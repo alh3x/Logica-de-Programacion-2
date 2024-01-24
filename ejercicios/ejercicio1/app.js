b65d1f5ce332c19571e5f01ed52f812573971cff
alex <oliver-pinto@outlook.com>
2024-01-24 20:31:35 -0300

    Aula 1 terminada

---
+++ ejercicios/ejercicio1/app.js
@@ -0,0 +1,26 @@
+/**Desafíos
+Descarga otro proyecto haciendo clic en este enlace y ábrelo en Visual Studio Code.
+Cambia el contenido de la etiqueta h1 con document.querySelector y asigna el siguiente texto: "Hora del Desafío".
+Crea una función que muestre en la consola el mensaje "El botón fue clicado" siempre que se presione el botón "Console".
+Crea una función que se ejecute cuando se haga clic en el botón "prompt", preguntando el nombre de una ciudad de Brasil. Luego, muestra una alerta con el mensaje concatenando la respuesta con el texto: "Estuve en {ciudad} y me acordé de ti".
+Crea una función que muestre una alerta con el mensaje: "Yo amo JS" siempre que se presione el botón "Alerta".
+Al hacer clic en el botón "suma", pide 2 números y muestra el resultado de la suma en una alerta. */
+let titulo = document.querySelector('h1');
+titulo.innerHTML = "Hora del Desafío";
+
+function consoleFunc() {
+    console.log("El botón fue clicado");
+};
+function promptFunc() {
+    let ciudad = prompt('Escribe el nombre de una ciudad de Brasil');
+    alert(`Estuve en ${ciudad} y me acordé de ti`);
+};
+function alertFunc() {
+    alert('Yo amo JS');
+};
+function sumaFunc() {
+    let numero1 = parseInt(prompt('Indique un numero para la suma'));
+    let numero2 = parseInt(prompt('Indique el segundo numero'));
+    let resultado = numero1 + numero2;
+    alert(`La suma de ${numero1} y ${numero2} es igual a ${resultado}`);
+};
---
+++ readme.md
@@ -0,0 +1,13 @@
+para interactuar con el dom podemos utilizar el document.querySelector('taghtml');
+para interactuar con botones se utilizan eventos, y estos eventos se utilizan en el html y siempre comienzan con el prefijo on...  onclick="alert('click')" como se ve pueden contener codigo js(no es lo ideal, es mejor que contengan funciones)
+
+# __Aula 1__
+
+Aquí tienes un resumen de los temas enseñados en esta aula:
+
+Cargamos el proyecto con HTML y CSS para crear la estructura y el estilo de la página que usaremos durante el curso.
+Aprendimos a manipular el contenido de los elementos HTML, como H1 y párrafos (p), utilizando JavaScript para cambiar dinámicamente el texto mostrado en la página.
+Comprendimos la importancia de la interacción entre HTML y JavaScript para crear páginas web dinámicas e interactivas.
+
+--------------------------------------------------------------------------------------------------------------------
+
